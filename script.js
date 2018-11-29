09a88fa40a06b2846b9c793d4e0f6bf0ef0229f3
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2018-11-29 15:52:02 -0500

    DONE! All looks good. Enjoy.

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 'use strict';
 
-var url = "https://api.github.com/users/";
+const url = "https://api.github.com/users/";
 // :username/repos - is endpoint
-var apiKey = "67649941d385b4e1860eb79d8568a696060554fb";
 
 
 function eventListener() {
   $('.search-form').submit(event => {
     event.preventDefault();
+    $('.repo-results').empty();
     let usernameInput = $('#user-search-input').val();
     let urlQuery = url + `${usernameInput}/repos`;
     callAPI(urlQuery);
-
   });
 }
 
 function callAPI(url) {
-  console.log(url);
+  const options = {
+    headers: new Headers({
+      'username': 'https://api.github.com',
+      'Accept': 'application/vnd.github.v3+json'
+    })
+  };
+  console.log(url, options);
   fetch(url)
-  .then(response => {
-    console.log(response.json());
-  })
-  .then(repsonseJSON => displayResults(responseJSON))
-  .catch( alert("Rugh rough."));
+  .then(response => response.json())
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => alert("Rugh rough."));
 }
 
 function displayResults(userResults) {
-    for (let i = 0; i <= responseJSON.length; i++) {
-      $('.repo-results').append(`<p>${userResults.id}</p>`);
+    console.log("We made it to display!")
+    for (let i = 0; i < userResults.length; i++) {
+      $('.repo-results').append(`<p><span class="repo-name">Repo Name:</span> <span>${userResults[i].name}</span><br/> <span class="url-label">URL: </span><span class="url">${userResults[i].owner.url}</span></p>`);
     }
 }
 $(eventListener());
